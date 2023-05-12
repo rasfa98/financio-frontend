27a6cadb6317c4367edc664a6fd868f1f36f8182
Rasmus Falk <rasmus@codebet.se>
2023-05-12 20:09:03 +0200

    Style changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,6 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @Input() isAuthenticated: boolean = false;
   @Input() email: string = '';
 }
